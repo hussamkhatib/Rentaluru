b73c6976b11cadd4d4847038acf4aad378417c45
hussamkhatib <hussamkhatib@gmail.com>
2022-10-14 20:35:47 +0530

    add an example that shows how to plot a polygon in map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,8 +1,29 @@
 import mapboxgl from "mapbox-gl";
 import { useState } from "react";
-import ReactMap from "react-map-gl";
+import ReactMap, { Source, Layer } from "react-map-gl";
+import { data } from "../temp";
 
 mapboxgl.accessToken = process.env.NEXT_PUBLIC_ACCESS_TOKEN || "";
+
+const geojson = {
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      geometry: { type: "Polygon", coordinates: data },
+    },
+  ],
+};
+
+const layerStyle = {
+  id: "area",
+  type: "fill",
+  paint: {
+    // "fill-opacity": 0.7,
+    "fill-outline-color": "rgb(52,51,50)",
+    "fill-color": "#007cbf",
+  },
+};
 
 const Map = () => {
   const [viewState, setViewState] = useState({
@@ -18,7 +39,11 @@ const Map = () => {
       style={{ width: "100vw", height: "100vh" }}
       mapboxAccessToken={mapboxgl.accessToken}
       mapStyle="mapbox://styles/mapbox/dark-v9"
-    />
+    >
+      <Source id="my-data" type="geojson" data={geojson}>
+        <Layer {...layerStyle} />
+      </Source>
+    </ReactMap>
   );
 };
 
