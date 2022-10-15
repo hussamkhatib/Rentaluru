bff8821784f8a8dd5d1ea196afbe9c4a3474c224
hussamkhatib <hussamkhatib@gmail.com>
2022-10-16 03:35:57 +0530

    add headlessui and use its disclosure component

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -4,6 +4,7 @@ import {
   selectFilterPanel,
   toggleFilterPanel,
 } from "../../redux/filterPanelSlice";
+import Disclosure from "../Disclosure";
 
 const Filter = () => {
   return (
@@ -24,7 +25,7 @@ const ToggleFilter = () => {
       <div className="w-8 rotate-90 text-white mt-16">
         <button
           onClick={() => dispatch(toggleFilterPanel())}
-          className="flex space-x-2 items-center"
+          className="flex space-x-2 items-center focus:outline-none focus-visible:ring focus-visible:ring-[#00FFA4] focus-visible:ring-opacity-75"
         >
           <PlayIcon className="h-4 w-4" aria-hidden />
           <span className="text-sm">Filters</span>
@@ -39,7 +40,11 @@ const ApplyFilter = () => {
 
   return filterPanel ? (
     <div className="fixed right-8 w-64 top-0 z-10 h-screen text-white bg-[#0D2337]">
-      The filter panel
+      <div className="mt-6" />
+      <Disclosure title="Rent">the children</Disclosure>
+      <Disclosure title="Rating">the children</Disclosure>
+      <Disclosure title="BHK">the children</Disclosure>
+      <Disclosure title="Vehicle">the children</Disclosure>
     </div>
   ) : null;
 };
