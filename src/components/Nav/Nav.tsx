3ef38512b38a9ed8969e2446ffdec7df5a718d24
hussamkhatib <hussamkhatib@gmail.com>
2022-11-07 11:26:24 +0530

    update .gitignore

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -3,7 +3,7 @@ import CommandPalette from "./CommandPallete";
 // import CommandPalette from "../CommandPalette";
 // import SelectMapType from "./SelectMapType";
 
-const Nav: FC = ({ onNavigate }: any) => {
+const Nav = ({ onNavigate }: any) => {
   return (
     <nav className="fixed w-full top-0 z-30 bg-[#0D2337] flex h-10 justify-between items-center px-2 md:px-4">
       <h1 className="text-xl text-white  font-semibold">Rentaluru</h1>
