4acfcd2a2c05b58d1c9b7d60b839f8a71d6a67a3
hussamkhatib <hussamkhatib@gmail.com>
2022-11-08 22:09:41 +0530

    re-enable toggle filter panel

--- src/features/filter/ApplyFilter.tsx
+++ src/features/filter/ApplyFilter.tsx
@@ -1,13 +1,17 @@
+import { selectFilterPanel } from "../../app/services/filterPanelSlice";
+import { useTypedSelector } from "../../app/store";
 import BHKFilter from "./BHKFilter";
 
 const ApplyFilter = () => {
-  return (
+  const filterPanel = useTypedSelector(selectFilterPanel);
+
+  return filterPanel ? (
     <div className="fixed right-8 w-64 top-10 z-10 h-screen text-white bg-[#0D2337]">
       <div className="mt-6  px-2">
         <BHKFilter />
       </div>
     </div>
-  );
+  ) : null;
 };
 
 export default ApplyFilter;
