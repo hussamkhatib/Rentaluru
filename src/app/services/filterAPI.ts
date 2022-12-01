22e1ab99b98211ba61c9ff0a05da557a294ff358
hussamkhatib <hussamkhatib@gmail.com>
2022-12-01 15:28:37 +0530

    add ssr

--- src/app/services/filterAPI.ts
+++ src/app/services/filterAPI.ts
@@ -1,10 +1,16 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { HYDRATE } from "next-redux-wrapper";
 
 export const filterApi = createApi({
   reducerPath: "filterApi",
   baseQuery: fetchBaseQuery({
     baseUrl: "/api/",
   }),
+  extractRehydrationInfo(action, { reducerPath }) {
+    if (action.type === HYDRATE) {
+      return action.payload[reducerPath];
+    }
+  },
   endpoints: (builder) => ({
     getGeojson: builder.query<any, any>({
       query: (queryParam) => ({
@@ -13,4 +19,11 @@ export const filterApi = createApi({
     }),
   }),
 });
-export const { useGetGeojsonQuery } = filterApi;
+
+export const {
+  useGetGeojsonQuery,
+  util: { getRunningOperationPromises },
+} = filterApi;
+
+// export endpoints for use in SSR
+export const { getGeojson } = filterApi.endpoints;
