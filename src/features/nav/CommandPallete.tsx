e899b95ab944dcc7e5e65caf16c0bafe784c9d6c
hussamkhatib <hussamkhatib@gmail.com>
2022-12-01 15:50:44 +0530

    perf: remove the on render fetch of areas in pallete

--- src/features/nav/CommandPallete.tsx
+++ src/features/nav/CommandPallete.tsx
@@ -6,11 +6,7 @@ import { setActiveArea } from "../leftPanel/leftPanelSlice";
 import { useAppDispatch } from "../../app/store";
 
 const CommandPalette = ({ onNavigate }: any) => {
-  const dispatch = useAppDispatch();
   const [isOpen, setIsOpen] = useState(false);
-  const [query, setQuery] = useState("");
-
-  const { data: filteredAreas } = useGetAreasQuery(query);
 
   useEffect(() => {
     const callback = (event: KeyboardEvent) => {
@@ -25,6 +21,34 @@ const CommandPalette = ({ onNavigate }: any) => {
     };
   }, []);
 
+  return (
+    <>
+      <button
+        className="border-2 inline-flex sm:w-40 items-center gap-x-2 text-sm py-1 px-4 text-white rounded-xl"
+        onClick={() => setIsOpen(true)}
+      >
+        <MagnifyingGlassIcon className="h-5 w-5" aria-hidden />
+        Ctrl + K
+      </button>
+      {isOpen && (
+        <Pallete
+          onNavigate={onNavigate}
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+        />
+      )}
+    </>
+  );
+};
+
+export default CommandPalette;
+
+const Pallete = ({ onNavigate, isOpen, setIsOpen }: any) => {
+  const [query, setQuery] = useState("");
+
+  const { data: filteredAreas } = useGetAreasQuery(query);
+  const dispatch = useAppDispatch();
+
   const handleNavigate = (area: any) => {
     setIsOpen(false);
     onNavigate({
@@ -35,89 +59,76 @@ const CommandPalette = ({ onNavigate }: any) => {
   };
 
   return (
-    <>
-      <button
-        className="border-2 inline-flex sm:w-40 items-center gap-x-2 text-sm py-1 px-4 text-white rounded-xl"
-        onClick={() => setIsOpen(true)}
+    <Transition.Root
+      show={isOpen}
+      as={Fragment}
+      afterLeave={() => setQuery("")}
+    >
+      <Dialog
+        onClose={setIsOpen}
+        className="fixed inset-0 z-20 p-4 pt-[25vh] overflow-y-auto"
       >
-        <MagnifyingGlassIcon className="h-5 w-5" aria-hidden />
-        Ctrl + K
-      </button>
-      <Transition.Root
-        show={isOpen}
-        as={Fragment}
-        afterLeave={() => setQuery("")}
-      >
-        <Dialog
-          onClose={setIsOpen}
-          className="fixed inset-0 z-20 p-4 pt-[25vh] overflow-y-auto"
+        <Transition.Child
+          enter="duration-300 ease-out"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="duration-200 ease-in"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
         >
-          <Transition.Child
-            enter="duration-300 ease-out"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="duration-200 ease-in"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
+          <Dialog.Overlay className="fixed inset-0 bg-gray-500/75" />
+        </Transition.Child>
+        <Transition.Child
+          enter="duration-300 ease-out"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="duration-200 ease-in"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          <Combobox
+            onChange={(area: any) => handleNavigate(area)}
+            as="div"
+            className="bg-white rounded-xl shadow-2xl max-w-xl relative mx-auto ring-1 ring-black/5 divide-y overflow-hidden"
           >
-            <Dialog.Overlay className="fixed inset-0 bg-gray-500/75" />
-          </Transition.Child>
-          <Transition.Child
-            enter="duration-300 ease-out"
-            enterFrom="opacity-0 scale-95"
-            enterTo="opacity-100 scale-100"
-            leave="duration-200 ease-in"
-            leaveFrom="opacity-100 scale-100"
-            leaveTo="opacity-0 scale-95"
-          >
-            <Combobox
-              onChange={(area: any) => handleNavigate(area)}
-              as="div"
-              className="bg-white rounded-xl shadow-2xl max-w-xl relative mx-auto ring-1 ring-black/5 divide-y overflow-hidden"
-            >
-              <div className="flex items-center px-4">
-                <MagnifyingGlassIcon
-                  className="h-6 w-6 text-gray-500"
-                  aria-hidden
-                />
-                <Combobox.Input
-                  onChange={(e) => {
-                    setQuery(e.target.value);
-                  }}
-                  className="w-full border-0 bg-transparent focus:ring-0 text-sm px-2 text-gray-800 placeholder:gray-400 h-12"
-                  placeholder="search"
-                />
-              </div>
-              {filteredAreas?.length > 0 ? (
-                <Combobox.Options
-                  static
-                  className="py-4 text-sm max-h-96 overflow-y-auto"
-                >
-                  {filteredAreas.map((area: any) => (
-                    <Combobox.Option value={area} key={area.name}>
-                      {({ active }) => (
-                        <div
-                          className={`px-4 py-2 w-full text-left ${
-                            active
-                              ? "bg-indigo-600 text-indigo-200"
-                              : "bg-white"
-                          }`}
-                        >
-                          {area.name}
-                        </div>
-                      )}
-                    </Combobox.Option>
-                  ))}
-                </Combobox.Options>
-              ) : (
-                <p className="text-sm p-4 text-gray-500">No search Results</p>
-              )}
-            </Combobox>
-          </Transition.Child>
-        </Dialog>
-      </Transition.Root>
-    </>
+            <div className="flex items-center px-4">
+              <MagnifyingGlassIcon
+                className="h-6 w-6 text-gray-500"
+                aria-hidden
+              />
+              <Combobox.Input
+                onChange={(e) => {
+                  setQuery(e.target.value);
+                }}
+                className="w-full border-0 bg-transparent focus:ring-0 text-sm px-2 text-gray-800 placeholder:gray-400 h-12"
+                placeholder="search"
+              />
+            </div>
+            {filteredAreas?.length > 0 ? (
+              <Combobox.Options
+                static
+                className="py-4 text-sm max-h-96 overflow-y-auto"
+              >
+                {filteredAreas.map((area: any) => (
+                  <Combobox.Option value={area} key={area.name}>
+                    {({ active }) => (
+                      <div
+                        className={`px-4 py-2 w-full text-left ${
+                          active ? "bg-indigo-600 text-indigo-200" : "bg-white"
+                        }`}
+                      >
+                        {area.name}
+                      </div>
+                    )}
+                  </Combobox.Option>
+                ))}
+              </Combobox.Options>
+            ) : (
+              <p className="text-sm p-4 text-gray-500">No search Results</p>
+            )}
+          </Combobox>
+        </Transition.Child>
+      </Dialog>
+    </Transition.Root>
   );
 };
-
-export default CommandPalette;
